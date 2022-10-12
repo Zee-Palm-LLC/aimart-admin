71aa96c96756f7d3b317a7fec96964804d6ccc1c
shamzeel-zeepalm <shamzeelirtaza@zeepalm.com>
2022-10-12 13:00:20 +0500

    initial Commit

---
+++ lib/app/data/theme.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'color.dart';
+
+ThemeData mainTheme = ThemeData(
+    fontFamily: GoogleFonts.plusJakartaSans().fontFamily,
+    textTheme: GoogleFonts.poppinsTextTheme(),
+    primaryColor: CustomColors.kPrimary,
+    primarySwatch: Colors.blue,
+    scaffoldBackgroundColor: CustomColors.kLightBackground,
+    textSelectionTheme: TextSelectionThemeData(
+        cursorColor: CustomColors.kPrimary,
+        selectionColor: CustomColors.kPrimary),
+    buttonTheme: ButtonThemeData(buttonColor: CustomColors.kPrimary),
+    appBarTheme: AppBarTheme(
+      iconTheme: IconThemeData(color: CustomColors.kDarkBblue),
+      color: CustomColors.kBackground,
+    ));
+
+SystemUiOverlayStyle defaultOverlay = const SystemUiOverlayStyle(
+    statusBarBrightness: Brightness.dark,
+    statusBarColor: Colors.transparent,
+    statusBarIconBrightness: Brightness.dark,
+    systemNavigationBarIconBrightness: Brightness.light,
+    systemNavigationBarDividerColor: Colors.black,
+    systemNavigationBarColor: Colors.black);
